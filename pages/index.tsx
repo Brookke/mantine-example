f4fcbaab055e9b2bd17d418a58523c084baf4cd0
Brooke Hatton <brooke@ravio.com>
2023-02-09 10:13:25 +0000

    add example of alignment not working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,42 @@
-import { Button, Group } from "@mantine/core";
+import { Table } from "@mantine/core";
+import styled from "@emotion/styled";
+
+const elements = [
+  { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
+  { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
+  { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
+  { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
+  { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
+];
 
 export default function IndexPage() {
+  const rows = elements.map((element) => (
+    <tr key={element.name}>
+      <td>{element.position}</td>
+      <td>{element.name}</td>
+      <td>{element.symbol}</td>
+      <td>{element.mass}</td>
+    </tr>
+  ));
+
   return (
-    <Group mt={50} position="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <Table>
+      <thead>
+        <tr>
+          {/**
+           * Align right Doesn't work as it gets overriden by the default styles in the Table component
+           */}
+          <StyledTh align="right">Element position</StyledTh>
+          <StyledTh>Element name</StyledTh>
+          <StyledTh>Symbol</StyledTh>
+          <StyledTh>Atomic mass</StyledTh>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
   );
 }
+
+const StyledTh = styled.th<{ align?: "left" | "right" }>({}, (props) => ({
+  textAlign: props.align,
+}));
